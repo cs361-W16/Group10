7a959dcd0d79a714ff67868538b95c0c869dd343
anisimon <anisimon@oregonstate.edu>
2016-01-22 19:54:25 -0800

    added a test for shuffle

--- Assignment1/src/test/java/Model/ModelTest.java
+++ Assignment1/src/test/java/Model/ModelTest.java
@@ -1,15 +1,17 @@
 package Model;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 /**
  * Created by Codysseus on 1/21/2016.
  */
 public class ModelTest {
 
-    @Test
+    /*@Test
     public void testHasFullDeck(){
         Deck stack = new Deck();
 
@@ -63,4 +65,33 @@ public class ModelTest {
         assertEquals('A', temp.getSuit());
         assertEquals(1, temp.getFace());
     }
+    */
+    /**
+     * Created by Natasha Anisimova
+     */
+    @Test
+    public void testshuffle(){
+        //Need code here to test the shuffle function, make sure the cards are not NULL
+        Deck tempdeck = new Deck();
+        //for (int i =0; i < 13; i++){
+        //    for(int i = 0; i < 4; i++){
+                //assertEquals(,temp.getSuit());
+        //Checking all the cards
+        for(int i=0; i <52; i++){
+            Card onecard = tempdeck.draw();
+            Assert.assertNotNull(onecard.getSuit());
+            Assert.assertNotNull(onecard.getFace());
+         //   }
+        }
+    }
+
+    //@Test
+    //public void testcardsinshuffle(){
+      //  Deck anothertempdeck = new Deck();
+        //for(int i=0; i<52; i++){
+            //Make sure that the suit is at least equal to one of the possible suits
+            //assertEquals("C" || "H" || "D" || "S",anothertempdeck.draw().getSuit());
+
+        //}
+    //}
 }
